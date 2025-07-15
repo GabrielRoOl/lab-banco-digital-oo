3fb892a0f4cd50c607e761b41145a1323e906656
GabrielRoOl <gabriel.ro.ol@hotmail.com>
2025-07-15 18:41:31 -0300

    tests started

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import bank.Bank;
+import bank.Client;
+
 public class Main {
     public static void main(String[] args) {
 
+        Bank santander = new Bank("santander", 1);
+
+
+        Client alex = new Client("Alex", "123", santander);
+
+        santander.openAccount(alex, "AS");
+
+        System.out.println(santander);
 
     }
 }
