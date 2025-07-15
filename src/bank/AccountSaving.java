02522e0b0c97fcbd3ea1924c9b454e777e1c96b1
GabrielRoOl <gabriel.ro.ol@hotmail.com>
2025-07-15 17:56:46 -0300

    feat: finalized AccountSaving class logic

--- src/bank/AccountSaving.java
+++ src/bank/AccountSaving.java
@@ -2,8 +2,24 @@ package bank;
 
 public class AccountSaving extends Count {
 
+    // É importante guardar a taxa como um decimal. Ex: 0.5% = 0.005
+    private double interestRate;
 
-    public AccountSaving(Client client, int agency, int number, double sale) {
-        super(client, agency, number, sale);
+    public AccountSaving(Client client, int agency, int number, double interestRate) {
+        super(client, agency, number);
+        this.interestRate = interestRate;
+    }
+
+    public void interestIncome(){
+        if(this.sale > 0){
+            double performance = this.sale * this.interestRate;
+            this.sale += performance;
+            System.out.printf("Yield amount: R$ %.2f\n", performance);
+            System.out.printf("New balance: R$ %.2f\n", this.sale);
+        }
+        else {
+            System.out.println("There is no balance to earn interest.");
+        }
+
     }
 }
