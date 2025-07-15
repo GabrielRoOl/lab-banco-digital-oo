6a14cae3ee7be5faa6713a5fc9d98e5a982650bd
GabrielRoOl <gabriel.ro.ol@hotmail.com>
2025-07-15 17:37:35 -0300

    feat: special withdrawal logic finalized

--- src/bank/AccountChecking.java
+++ src/bank/AccountChecking.java
@@ -11,6 +11,16 @@ public class AccountChecking extends Count {
 
     @Override
     public void sake(double sake){
+        double availableBalance = this.sale + this.specialCheckLimit;
+
+        if(sake > 0 && sake <= availableBalance){
+            this.sale -= sake;
+            System.out.println("Withdrawal of R$" + sake + " successfully completed (using overdraft).");
+            System.out.println("New balance: R$" + this.sale);
+        } else{
+            System.out.println("Operation not allowed. Withdrawal limit exceeded.");
+            System.out.println("Withdrawal amount: R$" + sake + ". Available balance (balance + limit): R$" + availableBalance);
+        }
 
     }
 
